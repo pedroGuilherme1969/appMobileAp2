9430935db90bc7f51f0bf8c8ad75ac24a2c1bedd
Pedro <pg20041312@gmail.com>
2024-06-03 15:00:49 -0300

    commit home-page

---
+++ app/src/main/java/com/example/barbershopap2/view/Home.kt
@@ -0,0 +1,12 @@
+package com.example.barbershopap2.view
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.barbershopap2.R
+
+class Home : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home2)
+    }
+}
