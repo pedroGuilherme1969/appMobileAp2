41df44a1611e497e15013a8f5001d7507845c817
Pedro <pg20041312@gmail.com>
2024-06-05 19:07:49 -0300

    app versão final

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.jetbrains.kotlin.android)
+
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -47,7 +49,10 @@ dependencies {
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
+    implementation(libs.firebase.firestore.ktx)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
+    implementation(platform("com.google.firebase:firebase-bom:33.1.0"))
 }
--- app/src/main/java/com/example/barbershopap2/view/Agendamento.kt
+++ app/src/main/java/com/example/barbershopap2/view/Agendamento.kt
@@ -1,20 +1,24 @@
 package com.example.barbershopap2.view
 
-import android.annotation.SuppressLint
-import android.icu.util.Calendar
-import android.os.Bundle
+
+import android.graphics.Color
 import android.os.Build
-import androidx.activity.enableEdgeToEdge
+import android.os.Bundle
+import android.view.View
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-import com.example.barbershopap2.R
 import com.example.barbershopap2.databinding.ActivityAgendamentoBinding
+import com.google.android.material.snackbar.Snackbar
+import com.google.firebase.firestore.FirebaseFirestore
+import java.util.Calendar
 
 class Agendamento : AppCompatActivity() {
 
     private lateinit var binding: ActivityAgendamentoBinding
-
+    private val calendar: Calendar = Calendar.getInstance()
+    private var data: String = ""
+    private var hora: String = ""
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityAgendamentoBinding.inflate(layoutInflater)
@@ -24,5 +28,96 @@ class Agendamento : AppCompatActivity() {
         val nome = intent.extras?.getString("nome").toString()
 
         val datePicker = binding.datePicker
+        datePicker.setOnDateChangedListener { _, year, monthOfYear, dayOfMonth ->
+
+            calendar.set(Calendar.YEAR,year)
+            calendar.set(Calendar.MONTH,monthOfYear)
+            calendar.set(Calendar.DAY_OF_MONTH,dayOfMonth)
+
+            var dia = dayOfMonth.toString()
+            val mes: String
+
+            if (dayOfMonth < 10){
+                dia = "0$dayOfMonth"
+
+            }
+            if (monthOfYear < 10){
+                mes = "" + (monthOfYear+1)
+            }else {
+                mes = (monthOfYear +1).toString()
+            }
+
+            data = "$dia / $mes / $year"
+        }
+
+        binding.timePicker.setOnTimeChangedListener { _, hourOfDay, minute ->
+
+            val minuto: String
+
+            if (minute < 10){
+                minuto = "0$minute"
+            }else{
+                minuto = minute.toString()
+            }
+
+            hora = "$hourOfDay:$minuto"
+        }
+        binding.timePicker.setIs24HourView(true)
+
+        binding.btAgendar.setOnClickListener {
+            val barbeiro1 = binding.barbeiro1
+            val barbeiro2 = binding.barbeiro2
+            val barbeiro3 = binding.barbeiro3
+
+            when {
+                hora.isEmpty() -> {
+                    mensagem(it, "Escolha um horário", "#FF0000")
+                }
+                hora < "8:00" && hora > "19:00" -> {
+                    mensagem(it, "Barbearia fechada - Horário de funcionamento das 08:00 as 19:00!", "#FF0000")
+                }
+                data.isEmpty() -> {
+                    mensagem(it, "Escolha uma data", "#FF0000")
+                }
+                barbeiro1.isChecked && data.isNotEmpty() && hora.isNotEmpty() -> {
+                    salvarAgendamento(it, nome, "Henrique Soares", data,hora)
+                }
+                barbeiro2.isChecked && data.isNotEmpty() && hora.isNotEmpty() -> {
+                    salvarAgendamento(it, nome, "Pedro Guilherme", data,hora)
+                }
+                barbeiro3.isChecked && data.isNotEmpty() && hora.isNotEmpty() -> {
+                    salvarAgendamento(it, nome, "Vinicius Maximo", data,hora)
+                }
+                else -> {
+                    mensagem(it, "Escolha um barbeiro", "#FF0000")
+                }
+            }
+        }
     }
+
+    private fun mensagem(view: View, mensagem: String, cor: String) {
+        val snackbar = Snackbar.make(view, mensagem, Snackbar.LENGTH_SHORT)
+        snackbar.setBackgroundTint(Color.parseColor(cor))
+        snackbar.setTextColor(Color.parseColor("#FFFFFF"))
+        snackbar.show()
+    }
+
+    private fun salvarAgendamento(view: View, cliente: String, barbeiro: String, data: String, hora: String){
+        val db = FirebaseFirestore.getInstance()
+
+        val dadosUsuario = hashMapOf(
+            "cliente" to cliente,
+            "barbeiro" to barbeiro,
+            "data" to data,
+            "hora" to hora
+        )
+
+        db.collection("agendamento").document(cliente).set(dadosUsuario).addOnCompleteListener {
+            mensagem(view, "Agendamento realizado com sucerro!", "#FF03DAC5")
+        }.addOnFailureListener {
+            mensagem(view, "O agendamento não pode ser concluído, erro no servidor ://", "FF0000")
+        }
+    }
+
+
 }
